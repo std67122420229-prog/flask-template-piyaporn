8cd3247cea9587cdfe412e8e1bacf398d3baf38d
std.67122420229@ubru.ac.th <std.67122420229@ubru.ac.th>
2025-12-16 13:07:50 +0700

    edit template files

--- server.py
+++ server.py
@@ -5,18 +5,25 @@ app = Flask(__name__)
 @app.route('/')
 def index():
     tiile = 'Home Page'
-    return render_template('index.html', tiile=tiile)
+    return render_template('index.html', title=tiile)
 
 @app.route('/about')
 def about():
     title = 'About Page'
     name = 'piaporn silsart'
-    email = 'std.67122420229'@ubru.ac.th'
+    email = 'std.67122420229@ubru.ac.th'
     mobile = '0979545891'
-    return render_template('about.html', title=title, name=name, email=email, mobile=mobile age=age)
+    age = 20
+    return render_template('about.html', title=title, name=name, email=email, mobile=mobile, age=age)
   
 @app.route('/favorite/foods')
 def favorite_foods():
     title = 'Favorite Foods'
     foods = ['Pizza', 'Sushi', 'Ice Cream']
     return render_template('favorite_foods.html', title=title, foods=foods)
+    
+@app.route('/favorite/sports')
+def favorite_sports():
+    title = 'Favorite Sports'
+    sports = ['ว่ายนํ้า', 'ฟุตบอล', 'วอลเล่ย์บอล']
+    return render_template('favorite_sports.html', title=title, sports=sports)
